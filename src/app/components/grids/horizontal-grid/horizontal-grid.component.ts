33e4e13d53ed86db840a88531370998ad192d569
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-01-09 01:50:49 +0100

    visual adjustments and prework

--- src/app/components/grids/horizontal-grid/horizontal-grid.component.ts
+++ src/app/components/grids/horizontal-grid/horizontal-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'asc-horizontal-grid',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./horizontal-grid.component.scss']
 })
 export class HorizontalGridComponent implements OnInit {
+
+  @Input() public headline: string;
+  @Input() public enableMore: boolean = false;
+  @Input() public canScroll: boolean = true;
 
   constructor() { }
 
