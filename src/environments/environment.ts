25ded01605f9f1d062740bd6380d1d4f42845ea0
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-01-22 14:07:10 +0100

    upgraded to https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  sso_base_uri: "http://api.tsalliance.eu/sso",
+  sso_base_uri: "https://api.tsalliance.eu/sso",
   sso_client_id: "2VYhnrdDzaEJlEyQ",
   sso_redirect_uri: "http://localhost:4200/authorize",
   api_base_uri: "http://localhost:3001",
