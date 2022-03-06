b8e2a02caa4f92ca77a4152d3bbedc94972f3d69
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-03-06 13:38:21 +0100

    added grid item template

---
+++ src/app/components/grid-items/grid-item-template/grid-item-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'asc-grid-item-template',
+  templateUrl: './grid-item-template.component.html',
+  styleUrls: ['./grid-item-template.component.scss']
+})
+export class GridItemTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
