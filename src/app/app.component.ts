0246c5ffeea234d2532e43c8d17f887ef73c7b40
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-03-30 09:51:36 +0200

    fixed: route loader not shown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,32 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { BehaviorSubject, filter, Observable } from 'rxjs';
 import { AuthenticationService } from 'src/sso/services/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
 
   public showSplashScreen: boolean = true;
-  public showLoadingBar: boolean = false;
+
+  private _loadingSubject: BehaviorSubject<boolean> = new BehaviorSubject(true);
+  public $isRouteLoading: Observable<boolean> = this._loadingSubject.asObservable();
 
   constructor(
     public authService: AuthenticationService, 
     private router: Router
   ) {
-
-    /*this.showSplashScreen = sessionStorage.getItem(SPLASH_SESSIONSTORAGE) === "true" || true;
-    sessionStorage.setItem(SPLASH_SESSIONSTORAGE, `${this.showSplashScreen}`);
-
-    this.authService.$isLoggedIn.subscribe((isLoggedIn) => this.showSplashScreen = !isLoggedIn)
-
     this.router.events.pipe(filter((event) => event instanceof NavigationStart || event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError)).subscribe((event: NavigationStart | NavigationEnd) => {
       if(event instanceof NavigationStart) {
-        this.showLoadingBar = true;
+        this._loadingSubject.next(true);
       } else {
-        this.showLoadingBar = false;
+        this._loadingSubject.next(false);
       }
-    })*/
-
+    })
   }
 
 
