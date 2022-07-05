c4f015ab06ae762461073b551bfd0f3bbd90a5a1
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-07-05 21:11:29 +0200

    overhauled drawer layout

--- projects/soundcore-ngx/src/lib/components/drawer/drawer.module.ts
+++ projects/soundcore-ngx/src/lib/components/drawer/drawer.module.ts
@@ -2,8 +2,8 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { SCNGXDrawerComponent } from './drawer.component';
 import { HeroIconModule, chevronRight } from 'ng-heroicon';
-import { SCNGXScreenModule } from "../../../services/screen/screen.module";
-import { SCNGXNavListItemModule } from "../../list/nav-list-item/nav-list-item.module";
+import { SCNGXScreenModule } from "../../services/screen/screen.module";
+import { SCNGXNavListItemModule } from "../list/nav-list-item/nav-list-item.module";
 
 @NgModule({
   declarations: [
