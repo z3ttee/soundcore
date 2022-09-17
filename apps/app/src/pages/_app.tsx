faf3b20b4bbfd9082b64be5c852cbf6e69e060ba
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-09-17 13:25:09 +0200

    requiring auth

--- apps/app/src/pages/_app.tsx
+++ apps/app/src/pages/_app.tsx
@@ -1,33 +1,49 @@
 import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
-import { SessionProvider, useSession } from "next-auth/react"
+import type { AppContext, AppProps } from 'next/app'
+import { getSession, SessionProvider, useSession } from "next-auth/react"
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import App from 'next/app';
+import { Session } from 'next-auth';
+import Head from 'next/head';
 
 interface MyAppProps extends AppProps<any> {
-
+  session?: Session;
 }
 
 function MyApp(props: MyAppProps) {
-  const { Component, pageProps } = props;
-  const router = useRouter();
-
-  useEffect(() => {
-    // if(!session && router.pathname !== "/auth/signin") {
-    //   console.log(router.pathname)
-    //   console.log(session)
-    //   router.push("/auth/signin")
-    // }
-  })
+  const { 
+    Component, 
+    pageProps, 
+    session 
+  } = props;
 
   // Fix type errors
   const PageComponent = Component as any;
 
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
+      <Head>
+        <title>Soundcore :: Web Player</title>
+        <meta name="description" content="Official TSAlliance private music streaming service" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
       <PageComponent {...pageProps} />
     </SessionProvider>
   );
+}
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  const { req } = context.ctx;
+  
+  const appProps = await App.getInitialProps(context)
+  const session = await getSession({ req })
+  
+  return {
+    ...appProps,
+    session
+  }
 }
 
 export default MyApp;
