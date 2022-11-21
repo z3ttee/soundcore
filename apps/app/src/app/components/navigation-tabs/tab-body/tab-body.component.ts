6dd5ba77fec5d1c30e52156be5ab1f7cbb709e67
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-11-21 23:01:38 +0100

    implemented tabs component

---
+++ apps/app/src/app/components/navigation-tabs/tab-body/tab-body.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "scngx-tab-body",
+    templateUrl: "./tab-body.component.html"
+})
+export class SCNGXTabBodyComponent {
+
+}
