18bed5ac9eab2a4437aa323f84c2476199abfe85
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-11-25 22:06:12 +0100

    optimized for onPush strategy

--- apps/app/src/app/modules/player/components/player-bar/player-bar.component.ts
+++ apps/app/src/app/modules/player/components/player-bar/player-bar.component.ts
@@ -1,15 +1,30 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnDestroy, ViewChild } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { Song } from "@soundcore/sdk";
-import { BehaviorSubject, debounce, debounceTime, Subject, takeUntil } from "rxjs";
+import { combineLatest, map, Observable, Subject, takeUntil, tap } from "rxjs";
 import { AppControlsService } from "../../services/controls.service";
 import { AppPlayerService } from "../../services/player.service";
 
+interface PlayerbarProps {
+    song?: Song;
+    currentTime?: number;
+    playing?: boolean;
+    responsive?: ResponsiveOptions;
+}
+
+interface ResponsiveOptions {
+    showDuration: boolean;
+    showMaximize: boolean;
+    showHistoryControls: boolean;
+    reducedMode: boolean;
+}
+
 @Component({
     selector: "scngx-player-bar",
-    templateUrl: "./player-bar.component.html"
+    templateUrl: "./player-bar.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppPlayerBarComponent implements OnInit, OnDestroy, AfterViewInit {
+export class AppPlayerBarComponent implements OnDestroy, AfterViewInit {
 
     @ViewChild("scngxPlayerContainer") private readonly container: ElementRef<HTMLDivElement>;
 
@@ -20,6 +35,9 @@ export class AppPlayerBarComponent implements OnInit, OnDestroy, AfterViewInit {
      */
     private readonly _destroy: Subject<void> = new Subject();
 
+    private readonly _responsiveness: Subject<ResponsiveOptions> = new Subject();
+    private readonly $responsiveness: Observable<ResponsiveOptions> = this._responsiveness;
+
     /**
      * ResizeObserver to observe viewport width changes.
      * This is used to trigger specific elements from becoming
@@ -27,69 +45,40 @@ export class AppPlayerBarComponent implements OnInit, OnDestroy, AfterViewInit {
      */
     private _observer: ResizeObserver;
 
-    public showMaximize: boolean = true;
-    public showHistoryControls: boolean = true;
-    public showDuration: boolean = true;
-    public reducedMode: boolean = false;
-
-    /**
-     * Current song state
-     */
-    public song?: Song;
-
-    /**
-     * Current time of the audio src.
-     */
-    public currentTime: number = 0;
-
-    /**
-     * Pause-State of playback
-     */
-    public isPaused: boolean = true;
-
-    public seekTime: number = 0;
-
     constructor(
         public readonly player: AppPlayerService,
         public readonly controls: AppControlsService
     ) {}
 
-    public ngOnInit(): void {
-        // Subscribe to changes on currently playing item
-        this.player.$current.pipe(takeUntil(this._destroy)).subscribe((current) => {
-            // Update internal song state to new playing item
-            this.song = current?.song;
-        });
-
-        // Subscribe to current time changes
-        this.player.$currentTime.pipe(takeUntil(this._destroy)).subscribe((currentTime) => {
-            // Update internal current time state
-            this.currentTime = currentTime ?? 0;
-            this.seekInputControl.setValue(this.currentTime)
-        });
-
-        // Subscribe to pause state changes
-        this.controls.$isPaused.pipe(takeUntil(this._destroy)).subscribe((isPaused) => {
-            // Update pause state
-            this.isPaused = isPaused;
-        });
-
-        this.seekInputControl.valueChanges.pipe(takeUntil(this._destroy), debounceTime(10)).subscribe((value) => {
-            // console.log(value);
-            // this.controls.seek(value);
+    public $props: Observable<PlayerbarProps> = combineLatest([
+        this.player.$current.pipe(takeUntil(this._destroy)),
+        this.player.$currentTime.pipe(takeUntil(this._destroy)),
+        this.controls.$isPaused.pipe(takeUntil(this._destroy)),
+        this.$responsiveness.pipe(takeUntil(this._destroy))
+    ]).pipe(
+        map(([currentItem, currentTime, isPaused, responsiveness]): PlayerbarProps => ({
+            song: currentItem?.song,
+            currentTime: currentTime ?? 0,
+            playing: !isPaused,
+            responsive: responsiveness
+        })),
+        tap((props) => {
+            this.seekInputControl.setValue(props.currentTime);
         })
-    }
+    );
 
     public ngAfterViewInit(): void {
         // Create ResizeObserver to react on viewport width changes.
         this._observer = new ResizeObserver((entries: ResizeObserverEntry[], observer: ResizeObserver) => {
             const width = this.container.nativeElement.getBoundingClientRect().width;
-      
-            this.showMaximize = width > 650;
-            this.showHistoryControls = width > 770;
-            this.showDuration = width > 650;
-            this.reducedMode = width < 600;
-        })
+
+            this._responsiveness.next({
+                reducedMode: width < 600,
+                showDuration: width > 650,
+                showHistoryControls: width > 770,
+                showMaximize: width > 650
+            });
+        });
       
         // Observe current component's html ref
         this._observer.observe(this.container.nativeElement);
