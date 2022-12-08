df8b65c78b20c4f28e61c5b36a0df7bdc2633223
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-12-08 20:01:02 +0100

    added first prototype

--- apps/app/src/app/modules/collection/collection.module.ts
+++ apps/app/src/app/modules/collection/collection.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CollectionComponent } from './views/collection/collection.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SCSDKCollectionModule } from '@soundcore/sdk';
+import { Error404Module } from 'src/app/shared/error404/error404.module';
+import { SCNGXSongListItemModule } from 'src/app/components/list-items/song-list-item/song-list-item.module';
+import { UiScrollModule } from 'ngx-ui-scroll';
+import { ListViewModule } from 'src/app/components/resource-views/list-view/list-view.module';
+import { SCNGXIconBtnModule } from '@soundcore/ngx';
 
 const routes: Routes = [
   { path: "", component: CollectionComponent }
@@ -13,7 +19,16 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+
+    Error404Module,
+    UiScrollModule,
+    ListViewModule,
+
+    SCNGXSongListItemModule,
+    SCNGXIconBtnModule,
+
+    SCSDKCollectionModule
   ]
 })
 export class CollectionModule { }
--- apps/app/src/app/modules/collection/views/collection/collection.component.ts
+++ apps/app/src/app/modules/collection/views/collection/collection.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SCNGXTracklist, SCNGXTracklistBuilder } from '@soundcore/ngx';
+import { LikedSong, SCSDKLikeService } from '@soundcore/sdk';
+import { SSOService } from '@soundcore/sso';
+import { combineLatest, map, Observable, of, Subject, switchMap, takeUntil, tap } from 'rxjs';
+import { AUDIOWAVE_LOTTIE_OPTIONS } from 'src/app/constants';
+import { PlayerItem } from 'src/app/modules/player/entities/player-item.entity';
+import { AppPlayerService } from 'src/app/modules/player/services/player.service';
+
+interface CollectionViewProps {
+  playing?: boolean;
+  currentItem?: PlayerItem;
+
+  tracklist?: SCNGXTracklist<LikedSong>;
+  $size?: Observable<number>;
+}
 
 @Component({
-  selector: 'app-collection',
   templateUrl: './collection.component.html',
-  styleUrls: ['./collection.component.scss']
+  styleUrls: ['./collection.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CollectionComponent implements OnInit {
+export class CollectionComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private readonly _destroy: Subject<void> = new Subject();
+  private readonly _destroySize: Subject<void> = new Subject();
+
+   // Lottie animations options
+  public animOptions = AUDIOWAVE_LOTTIE_OPTIONS;
+
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly likeService: SCSDKLikeService,
+    private readonly player: AppPlayerService,
+    private readonly tracklistBuilder: SCNGXTracklistBuilder,
+    private readonly ssoService: SSOService
+  ) { }
+
+  public readonly $props: Observable<CollectionViewProps> = combineLatest([
+    this.player.$current.pipe(takeUntil(this._destroy)),
+    this.player.$isPaused.pipe(takeUntil(this._destroy)),
+    this.ssoService.$user.pipe(
+      map((ssoUser) => {
+        return this.tracklistBuilder.forLikedSongs(ssoUser.id) as SCNGXTracklist<LikedSong>;
+      }),
+      map((tracklist): [SCNGXTracklist, Observable<number>] => {
+        this._destroySize.next();
+        return [tracklist, tracklist.$size.pipe(takeUntil(this._destroySize))];
+      }),
+      takeUntil(this._destroy)
+    )
+  ]).pipe(
+    map(([currentItem, isPaused, [tracklist, sizeObs]]): CollectionViewProps => ({
+      currentItem,
+      playing: !isPaused && currentItem?.tracklist?.assocResId == tracklist?.assocResId,
+      tracklist,
+      $size: sizeObs
+    })),
+    tap((props) => console.log(props))
+  );
 
   ngOnInit(): void {
+  }
+
+  public ngOnDestroy(): void {
+      this._destroy.next();
+      this._destroy.complete();
+
+      this._destroySize.next();
+      this._destroySize.complete();
+  }
+
+  public forcePlay(tracklist: SCNGXTracklist) {
+    this.player.playTracklist(tracklist, true);
   }
 
 }
