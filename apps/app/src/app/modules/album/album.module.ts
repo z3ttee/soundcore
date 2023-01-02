4c4a42f963d0518746e9c695833e3f5bd0182ea4
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2023-01-02 14:35:00 +0100

    fixed virtual scrolling

--- apps/app/src/app/modules/album/album.module.ts
+++ apps/app/src/app/modules/album/album.module.ts
@@ -7,7 +7,6 @@ import { SCDKAlbumModule } from '@soundcore/sdk';
 import { SCNGXAlbumGridItemModule, SCNGXArtworkModule, SCNGXHorizontalListModule, SCNGXIconBtnModule, SCNGXScrollingModule, SCNGXSongDurationPipeModule, SCNGXTableModule, SCNGXUiRowModule } from '@soundcore/ngx';
 import { HeroIconModule, heart, dotsVertical } from 'ng-heroicon';
 import { MatRippleModule } from '@angular/material/core';
-import { VirtualScrollerModule } from '@tsalliance/ngx-virtual-scroller';
 import { Error404Module } from 'src/app/shared/error404/error404.module';
 import { SCNGXSongListItemModule } from 'src/app/components/list-items/song-list-item/song-list-item.module';
 
@@ -23,7 +22,6 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    VirtualScrollerModule,
     ListViewModule,
     HeroIconModule.withIcons({ heart, dotsVertical }),
 
