396b9d6e17681e08e6b35d1141b318bb595300ca
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2023-01-11 20:11:59 +0100

    optimized search page for mobile

--- apps/app/src/app/modules/search/views/search-index/search-index.component.ts
+++ apps/app/src/app/modules/search/views/search-index/search-index.component.ts
@@ -1,16 +1,34 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { UntypedFormControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { BehaviorSubject, debounceTime, Observable, Subject, takeUntil } from 'rxjs';
+import { combineLatest, debounceTime, Observable, Subject, switchMap, takeUntil, tap } from 'rxjs';
 import { SCCDKScreenService } from '@soundcore/cdk';
-import { SCDKGenreService, MeiliAlbum, MeiliArtist, SCSDKSearchService, ComplexSearchResult, SCDKResource, ApiSearchResponse, MeiliPlaylist, Pageable, MeiliUser, SCDKUserService, SCDKArtistService, SCDKAlbumService, Genre, MeiliSong, SCSDKSongService, SCSDKPlaylistService } from '@soundcore/sdk';
+import { SCDKGenreService, MeiliAlbum, MeiliArtist, SCSDKSearchService, SCDKResource, MeiliPlaylist, Pageable, MeiliUser, SCDKUserService, SCDKArtistService, SCDKAlbumService, MeiliSong, SCSDKSongService, SCSDKPlaylistService } from '@soundcore/sdk';
 import { MatSnackBar } from '@angular/material/snack-bar';
+
+interface SearchIndexProps {
+  query?: string;
+  history?: SCDKResource[];
+
+  hits?: SearchHits;
+
+  isTouch?: boolean;
+}
+
+interface SearchHits {
+  songs?: MeiliSong[];
+  artists?: MeiliArtist[];
+  albums?: MeiliAlbum[];
+  playlists?: MeiliPlaylist[];
+  users?: MeiliUser[];
+}
 
 @Component({
   selector: 'app-search-index',
   templateUrl: './search-index.component.html',
-  styleUrls: ['./search-index.component.scss']
+  styleUrls: ['./search-index.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SearchIndexComponent implements OnInit, OnDestroy {
 
@@ -26,78 +44,63 @@ export class SearchIndexComponent implements OnInit, OnDestroy {
     private readonly songService: SCSDKSongService,
     private readonly playlistService: SCSDKPlaylistService,
 
-    public readonly screenService: SCCDKScreenService,
+    private readonly screenService: SCCDKScreenService,
     private readonly snackbar: MatSnackBar
   ) { }
 
-  private readonly _destroy: Subject<void> = new Subject();
-  private readonly _cancelQuery: Subject<void> = new Subject();
+  private readonly $destroy: Subject<void> = new Subject();
+  private readonly $cancel: Subject<void> = new Subject();
 
-  private readonly _resultSubject: BehaviorSubject<ComplexSearchResult> = new BehaviorSubject(null);
-  public readonly $recently: Observable<SCDKResource[]> = this.searchService.$recentlySearched.pipe(takeUntil(this._destroy));
-
-  public show404: boolean = false;
-  public query: string = "";
   public readonly searchInputControl: UntypedFormControl = new UntypedFormControl("");
 
-  public $playlists: BehaviorSubject<ApiSearchResponse<MeiliPlaylist>> = new BehaviorSubject(null);
-  public $users: BehaviorSubject<ApiSearchResponse<MeiliUser>> = new BehaviorSubject(null);
-  public $artists: BehaviorSubject<ApiSearchResponse<MeiliArtist>> = new BehaviorSubject(null);
-  public $albums: BehaviorSubject<ApiSearchResponse<MeiliAlbum>> = new BehaviorSubject(null);
-  public $songs: BehaviorSubject<ApiSearchResponse<MeiliSong>> = new BehaviorSubject(null);
+  public $props: Observable<SearchIndexProps> = combineLatest([
+    this.searchService.$recentlySearched.pipe(takeUntil(this.$destroy)),
+    this.searchService.$onMainInput.pipe(debounceTime(300), tap((_) => this.cancelCurrentQuery()), takeUntil(this.$destroy)),
+    this.screenService.$isTouch.pipe(takeUntil(this.$destroy))
+  ]).pipe(
+    switchMap(([history, query, isTouch]) => {
+      return new Observable<SearchIndexProps>((subscriber) => {
+        subscriber.add(combineLatest([
+          this.songService.searchSongs(query, new Pageable(0, 10)).pipe(takeUntil(this.$cancel)),
+          this.artistService.searchArtist(query, new Pageable(0, 10)).pipe(takeUntil(this.$cancel)),
+          this.albumService.searchAlbum(query, new Pageable(0, 10)).pipe(takeUntil(this.$cancel)),
+          this.playlistService.searchPlaylist(query, new Pageable(0, 10)).pipe(takeUntil(this.$cancel)),
+          this.userService.searchUser(query, new Pageable(0, 10)).pipe(takeUntil(this.$cancel))
+        ]).subscribe(([songSearchReq, artistSearchReq, albumSearchReq, playlistSearchReq, userSearchReq]) => {
+          const hits: SearchHits = {
+            songs: songSearchReq?.payload?.hits ?? [],
+            artists: artistSearchReq?.payload?.hits ?? [],
+            albums: albumSearchReq?.payload?.hits ?? [],
+            playlists: playlistSearchReq?.payload?.hits ?? [],
+            users: userSearchReq?.payload?.hits ?? []
+          }
+          
+          subscriber.next({
+            hits: hits,
+            query: query,
+            history: history,
+            isTouch: isTouch
+          });
+          subscriber.complete();
+        }))
+      })
+    })
+  );
+
 
   public ngOnInit(): void {
-    this.searchService.$onMainInput.pipe(debounceTime(300), takeUntil(this._destroy)).subscribe((query) => {
-      this._cancelQuery.next();
-      this.query = query;
-
-      if(typeof query == "undefined" || query == null || query.replace(/\s/g, '') == "") {
-        this._resultSubject.next(null);
-        this.show404 = false;
-        return
-      }
-
-      // Search songs
-      this.songService.searchSongs(query, new Pageable(0, 10)).pipe(takeUntil(this._cancelQuery)).subscribe((response) => {
-        this.$songs.next(response.payload);
-      })
-
-      // Search artists
-      this.artistService.searchArtist(query, new Pageable(0, 10)).pipe(takeUntil(this._cancelQuery)).subscribe((response) => {
-        this.$artists.next(response.payload);
-      })
-
-      // Search albums
-      this.albumService.searchAlbum(query, new Pageable(0, 10)).pipe(takeUntil(this._cancelQuery)).subscribe((response) => {
-        this.$albums.next(response.payload);
-      })
-
-      // Search playlists
-      this.playlistService.searchPlaylist(query, new Pageable(0, 10)).pipe(takeUntil(this._cancelQuery)).subscribe((response) => {
-        this.$playlists.next(response.payload);
-      })
-
-
-
-      // Search users
-      this.userService.searchUser(query, new Pageable(0, 10)).pipe(takeUntil(this._cancelQuery)).subscribe((response) => {
-        this.$users.next(response.payload);
-      })
-
-    })
-
     // Push queries that were received from input on mobile devices.
-    this.searchInputControl.valueChanges.pipe(takeUntil(this._destroy)).subscribe((value) => {
+    this.searchInputControl.valueChanges.pipe(takeUntil(this.$destroy)).subscribe((value) => {
       this.searchService.emitMainInput(value);
     })
   }
 
   public ngOnDestroy(): void {
-    this._destroy.next();
-    this._destroy.complete();
+    this.$destroy.next();
+    this.$destroy.complete();
 
-    this._cancelQuery.next();
-    this._cancelQuery.complete();
+    this.$cancel.next();
+    this.$cancel.complete();
   }
 
   public routeToResult(route: any[], resource: SCDKResource) {
@@ -106,13 +109,17 @@ export class SearchIndexComponent implements OnInit, OnDestroy {
   }
 
   public removeFromSearch(song: MeiliSong) {
-    this.searchService.removeFromHistory(song).pipe(takeUntil(this._destroy)).subscribe((wasRemoved) => {
+    this.searchService.removeFromHistory(song).pipe(takeUntil(this.$destroy)).subscribe((wasRemoved) => {
       if(wasRemoved) {
         this.snackbar.open(`Eintrag aus Suchverlauf gelöscht`, null, { duration: 5000 });
       } else {
         this.snackbar.open(`Es ist ein Fehler aufgetreten`, null, { duration: 5000 });
       }
     });
+  }
+
+  private cancelCurrentQuery() {
+    this.$cancel.next();
   }
 
 }
