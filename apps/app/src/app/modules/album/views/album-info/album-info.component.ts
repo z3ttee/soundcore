60aa96d906f91bbf50dcaf9fc5448504932f2df4
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2023-03-29 15:29:49 +0200

    decoupled some observables for faster loading times

--- apps/app/src/app/modules/album/views/album-info/album-info.component.ts
+++ apps/app/src/app/modules/album/views/album-info/album-info.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest, map, Observable, of, Subject, switchMap, takeUntil } from 'rxjs';
-import { Album, ApiError, Future, SCDKAlbumService, SCSDKDatasource, SCSDKSongService, SCSDKTracklistV2Service, Song, toFutureCompat } from '@soundcore/sdk';
+import { combineLatest, map, Observable, of, startWith, Subject, switchMap, takeUntil } from 'rxjs';
+import { Album, Future, SCDKAlbumService, SCSDKDatasource, SCSDKSongService, SCSDKTracklistV2Service, Song, toFutureCompat } from '@soundcore/sdk';
 import { AUDIOWAVE_LOTTIE_OPTIONS } from 'src/app/constants';
 import { PlayerService } from 'src/app/modules/player/services/player.service';
 import { SCNGXTracklist } from 'src/app/modules/player/entities/tracklist.entity';
@@ -10,17 +10,12 @@ import { HttpClient } from '@angular/common/http';
 import { Page } from '@soundcore/common';
 
 interface AlbumInfoProps {
-  album?: Album;
-  loading?: boolean;
-  error?: ApiError;
-
-  currentlyPlaying?: any;
-
+  album?: Future<Album>;
   tracklist?: Future<SCNGXTracklist<Song>>;
-  datasource?: Future<SCSDKDatasource<Song>>;
   recommendedAlbums?: Future<Page<Album>>;
-
-  playing?: boolean;
+  isPlaying?: boolean;
+  isTracklistActive?: boolean;
+  currentItemId?: string;
 }
 
 @Component({
@@ -50,33 +45,34 @@ export class AlbumInfoComponent implements OnInit, OnDestroy {
   public $albumId: Observable<string> = this.activatedRoute.paramMap.pipe(map((params) => params.get("albumId")));
 
   /**
+   * Observable that emits current datasource
+   * of tracks
+   */
+  public $datasource: Observable<SCSDKDatasource<Song>> = this.$albumId.pipe(switchMap((albumId) => {
+    return this.songService.findByAlbumDatasource(albumId);
+  }))
+
+  /**
    * Observable that emits current
    * album data in future format
    */
   public $album: Observable<Future<Album>> = this.$albumId.pipe(switchMap((albumId) => this.albumService.findById(albumId).pipe(toFutureCompat())));
 
+  /**
+   * Observable that emits recommended albums
+   */
+  public $recommendedAlbums: Observable<Future<Page<Album>>> = this.$album.pipe(switchMap((album) => {
+    if(album.loading) return of(Future.loading());
+    return this.albumService.findRecommendedByArtist(album.data?.primaryArtist?.id, [ album.data?.id ]).pipe(toFutureCompat(),);
+  }))
+
   public readonly $props: Observable<AlbumInfoProps> = combineLatest([
     // Get changes to album
     this.$album,
-    // Load recommended albums by artist
-    this.$album.pipe(
-      switchMap((album) => {
-        // If album still loading, return future with state loading
-        if(album.loading) return of(Future.loading());
-
-        const data = album.data;
-        return this.albumService.findRecommendedByArtist(data?.primaryArtist?.id, [ data?.id ]).pipe(toFutureCompat(),);
-      })
-    ),
-    // Load datasource
-    this.$album.pipe(
-      switchMap((album): Observable<Future<SCSDKDatasource<Song>>> => {
-        // If album still loading, return future with state loading
-        if(album.loading) return of(Future.loading());
-        // Init datasource
-        return this.songService.findByAlbumDatasource(album.data?.id).pipe(map((datasource) => Future.of(datasource)));
-      })
-    ),
+    // Get paused state
+    this.playerService.$isPaused.pipe(startWith(true)), 
+    // Get current tracklist id
+    this.playerService.$currentItem.pipe(startWith(null)),
     // Load tracklist
     this.$album.pipe(
       switchMap((album): Observable<Future<SCNGXTracklist<Song>>> => {
@@ -92,24 +88,19 @@ export class AlbumInfoComponent implements OnInit, OnDestroy {
           })
         );
       })
-    )
-    // this.player.$current.pipe(takeUntil(this._destroy)),
-    // this.player.$isPaused.pipe(takeUntil(this._destroy))
+    ),
   ]).pipe(
     // Build props object
-    map(([album, recommendedAlbums, datasource, tracklist]): AlbumInfoProps => {
-      // const tracklist = future.data[0];
-      // const albums = future.data?.[1];
+    map(([album, isPaused, currentItem, tracklist]): AlbumInfoProps => {
+      const currentTracklistId = currentItem?.tracklistId;
+      const currentItemId = currentItem?.id;
 
       return {
-        loading: album.loading,
-        album: album.data,
+        album: album,
         tracklist: tracklist,
-        datasource: datasource,
-        // currentlyPlaying: currentItem,
-        // playing: !isPaused && currentItem?.tracklist?.id == tracklist?.id,
-        // tracklist: tracklist,
-        recommendedAlbums: recommendedAlbums
+        isPlaying: !isPaused && currentTracklistId === tracklist.data?.id,
+        isTracklistActive: currentTracklistId === tracklist.data?.id,
+        currentItemId: currentItemId
       };
     }),
     takeUntil(this.$destroy)
