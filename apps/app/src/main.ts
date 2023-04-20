f18f2a5a575ffaa3ef7a0eb27d534e95271c6324
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2023-04-20 16:51:05 +0200

    added analytics

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -2,8 +2,12 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { inject } from '@vercel/analytics';
 
 if (environment.production) {
+  // Inject vercel analytics tool
+  inject();
+
   enableProdMode();
 }
 
