f849ae429f2c7eb4bd638dcc7dad47052ed35c35
z3ttee <cedric.zitzmann@gmail.com>
2025-04-01 13:04:16 +0200

    removed old package

--- packages/soundcore-cdk/projects/soundcore-cdk/src/lib/screen/entities/screen.entity.ts
+++
@@ -1,9 +0,0 @@
-export class SCCDKScreen {
-
-    constructor(
-        public readonly name: string,
-        public readonly isMobile: boolean,
-        public readonly width: number
-    ) {}
-
-}
--- packages/soundcore-cdk/projects/soundcore-cdk/src/lib/screen/screen.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { SCCDKScreenService } from "./screen.service";
-
-@NgModule({
-    providers: [
-        SCCDKScreenService
-    ]
-})
-export class SCCDKScreenModule {}
