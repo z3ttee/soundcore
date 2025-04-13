f7a955dffe6f1921aa75ce307e1a6ece1e9b6ac5
z3ttee <cedric.zitzmann@gmail.com>
2025-04-13 11:22:31 +0200

    added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Options} */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  endOfLine: "auto",
+  bracketSpacing: true,
+  arrowParens: "always",
+  bracketSameLine: true,
+  printWidth: 120
+};
+
+module.exports = config;
